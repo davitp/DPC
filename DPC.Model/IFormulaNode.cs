3520a780106d069951eae04958133eabfd299c19
davitp <davit.petrosyan.sc@gmail.com>
2016-04-13 21:39:30 +0400

    Commiting predicate, expression tree node and formula definitions.

---
+++ DPC.Model/IFormulaNode.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace DPC.Model
+{
+    /// <summary>
+    /// Predicate tree node interface
+    /// </summary>
+    public interface IFormulaNode
+    {
+        /// <summary>
+        /// Parent Node
+        /// </summary>
+        IFormulaNode Parent { get; set; }
+
+        /// <summary>
+        /// Child nodes 
+        /// </summary>
+        IEnumerable<IFormulaNode> Children { get; set; } 
+    }
+}
---
+++ DPC.Model/Predicate.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace DPC.Model
+{
+    /// <summary>
+    /// Class defines predicate
+    /// </summary>
+    public class Predicate : IFormulaNode
+    {
+        /// <summary>
+        /// Children of predicate
+        /// </summary>
+        public IEnumerable<IFormulaNode> Children { get; set; }
+
+        /// <summary>
+        /// Dimention of predicate
+        /// How many arguments can be be placed into the predicate
+        /// </summary>
+        public int Dimention { get; set; }
+
+        /// <summary>
+        /// Parent node in tree
+        /// </summary>
+        public IFormulaNode Parent { get; set; }
+
+    }
+}
